5b1375a3bbc253f219568cb47f6735eb2742765d
d4rk5ide-921 <abdullahumairtown@gmail.com>
2026-05-06 14:57:24 +0500

    fix: make backend vercel compatible

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import dns from "node:dns/promises";
 import ConnectionDB from "./DB/Connection.js";
 import todo from "./routes/todo.js";
 import Student from "./routes/student.js";
@@ -12,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" })); // Vercel pe localhost:5173 nahi hoga
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -23,54 +22,9 @@ app.use("/student", Student);
 app.use("/user", registers);
 app.use("/cards", cards);
 
-// app.post("/register", async (req, res) => {
-//   const { title, description } = req.body;
-
-//   if (!description) {
-//     return res.send("Please enter the description!");
-//   }
-
-//   if (!title) {
-//     return res.send("Please enter the title!");
-//   }
-
-//   const user = await User.findOne({ title, description });
-
-//   if (user) {
-//     return res.send("List already exists");
-//   }
-
-//   const todo = new User({ title, description });
-//   await todo.save();
-
-//   res.send("List registered successfully");
-// });
-// app.post("/update", async (req, res) => {
-//   const { id, title, description } = req.body;
-//   const user = await User.findByIdAndUpdate(
-//     { _id: id },
-//     { title, description },
-//   );
-//   res.send("List is Updated Successfully!");
-// });
-// app.delete("/delete", async (req, res) => {
-//   const { id, title, description } = req.body;
-//   const user = await User.findByIdAndDelete(
-//     { _id: id },
-//     { title, description },
-//   );
-//   res.send("List is Deleted Successfully!");
-// });
-// app.get("/register", async (req, res) => {
-//   const user = await User.find();
-//   res.send(user);
-// });
-dns.setServers(['8.8.8.8', '1.1.1.1']);
-
-
-
-
 ConnectionDB();
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+
+// Ye line hata do: app.listen(5000, ...)
+
+// Ye add karo sab se end me:
+export default app;
